1c742c68e46f176abd3e948b78414b074828d31a
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-05 17:25:49 +0530

    css

--- src/QR_Static.jsx
+++ src/QR_Static.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const QR_Static = () => {
   return (
-    <div className="w-[100%] h-[30vh] flex justify-center">
+    <div className="w-[100%] h-[30vh] flex justify-center mt-[36px] bg-red-600">
       <img
         src="https://user-images.githubusercontent.com/901732/38602308-9d811622-3d27-11e8-9a09-aacaa07df014.png"
         className="w-[200px] h-[200px]"
