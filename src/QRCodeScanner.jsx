dc126f2dfb94d572055432852d58a1ebdd714869
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-05 16:20:50 +0530

    modify

--- src/QRCodeScanner.jsx
+++ src/QRCodeScanner.jsx
@@ -1,24 +1,67 @@
+// import React, { useRef, useEffect } from "react";
+// import QrScanner from "qr-scanner"; // Import the qr-scanner library
+// import "qr-scanner/qr-scanner-worker.min"; // Import the worker script
+
+// const QRCodeScanner = ({ onScan, isScanning }) => {
+//   const videoRef = useRef(null);
+
+//   useEffect(() => {
+//     const scanner = new QrScanner(videoRef.current, (result) => {
+//       if (result) {
+//         console.log("QR Code detected:", result);
+//         onScan(result);
+//       }
+//     });
+
+//     if (isScanning) {
+//       scanner.start();
+//     }
+
+//     return () => {
+//       scanner.destroy();
+//     };
+//   }, [onScan, isScanning]);
+
+//   return (
+//     <div className="qrContainer">
+//       <video ref={videoRef} className="videoFullHeight" />
+//     </div>
+//   );
+// };
+
+// export default QRCodeScanner;
+
+
+
 import React, { useRef, useEffect } from "react";
 import QrScanner from "qr-scanner"; // Import the qr-scanner library
 import "qr-scanner/qr-scanner-worker.min"; // Import the worker script
 
 const QRCodeScanner = ({ onScan, isScanning }) => {
   const videoRef = useRef(null);
+  const scannerRef = useRef(null);
 
   useEffect(() => {
-    const scanner = new QrScanner(videoRef.current, (result) => {
-      if (result) {
-        console.log("QR Code detected:", result);
-        onScan(result);
-      }
-    });
+    if (!scannerRef.current) {
+      scannerRef.current = new QrScanner(videoRef.current, (result) => {
+        if (result) {
+          console.log("QR Code detected:", result);
+          onScan(result);
+        }
+      });
+    }
 
     if (isScanning) {
-      scanner.start();
+      scannerRef.current.start();
+    } else {
+      scannerRef.current.stop();
     }
 
     return () => {
-      scanner.destroy();
+      if (scannerRef.current) {
+        scannerRef.current.destroy();
+        scannerRef.current = null;
+      }
     };
   }, [onScan, isScanning]);
 
