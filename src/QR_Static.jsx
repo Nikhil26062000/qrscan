330f8ea29049c2a0fd68037d7e28c3663532bbd2
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-05 17:09:32 +0530

    css

--- src/QR_Static.jsx
+++ src/QR_Static.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const QR_Static = () => {
   return (
-    <div>
-     <p>QR Code has been scanned. Display static content here.</p>
+    <div className="w-[100%] h-[30vh] flex justify-center">
+      <img
+        src="https://user-images.githubusercontent.com/901732/38602308-9d811622-3d27-11e8-9a09-aacaa07df014.png"
+        className="w-[200px] h-[200px]"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default QR_Static
+export default QR_Static;
