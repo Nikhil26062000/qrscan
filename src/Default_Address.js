ce8b1ab63d03cd16b285220db41e77d99f55c933
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-07 20:19:25 +0530

    css

--- src/Default_Address.js
+++ src/Default_Address.js
@@ -16,7 +16,7 @@ const Default_Address = () => {
       <section className=" w-[318px] h-[569px] mx-auto mt-[28px] flex flex-col gap-[328px]">
         <section className="w-[318px] h-[185px] flex flex-col gap-[18px] ">
           <p className="font-[600] text-[24px] leading-[26.4px] text-[#125B57]">
-            Your Address
+            Your Address testing
           </p>
           <p className="font-inter font-[500] text-[16px] leading-[16px] text-[#125B57]">
             Default Address<span className="text-red-500 pl-[2px]">*</span>
