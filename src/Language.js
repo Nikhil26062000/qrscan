38be9ae9b0bf85a4cd298baa78cc1531cb3fe209
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-07 21:06:32 +0530

    css

--- src/Language.js
+++ src/Language.js
@@ -5,7 +5,7 @@ import Language_Component from './components/Language_Component'
 
 const Language = () => {
   return (
-    <div className=" w-[360px]   mx-auto">
+    <div className=" w-[100%]   mx-auto">
       <Top_Header title="Language" />
       <section className="w-[314px] h-[94px] mt-[134px] mx-auto rounded-[10px] bg-[#125B57]">
         <Language_Component language="English"/>
